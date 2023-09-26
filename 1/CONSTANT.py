3a9da33df94a98b9471cbf2b7f6494e93bdc90c7
TT Minh <trinhtheminh1104@gmail.com>
2023-09-26 23:04:02 +0700

    add comment to mini-project 1

--- 1/CONSTANT.py
+++ 1/CONSTANT.py
@@ -1,8 +1,10 @@
+# class Staff represent ID of staff and list of days off of that staff
 class Staff:
     def __init__(self, ID, dayoff):
         self.ID = ID
         self.dayoff = dayoff
     
+    #printing for debug
     def __str__(self):
         ans = f"Staff {self.ID}: "
         for day in self.dayoff:
@@ -11,9 +13,12 @@ class Staff:
         return ans 
 
 
+#import data
 def import_data(file):
-    staff = []
+    #list of staff
+    staff: list[Staff] = []
     with open(file, 'r') as f:
+        #N: number of Staff, D: number of Day, each slot in each day have at least A Staff and at most B staff
         N, D, A, B = map(int, f.readline().split())
         for i in range(N):
             F = list(map(int, f.readline().split()))
--- 1/main.py
+++ 1/main.py
@@ -1,24 +1,30 @@
 from CONSTANT import Staff, import_data
 
+#class Solver, solve the problem
 class Solver:
-	def __init__(self, staff, D, A, B):
+	def __init__(self, staff: list[Staff], D, A, B):
 		self.N = len(staff)
-		self.staff: list[Staff] = staff
+		self.staff = staff
 		self.D = D
 		self.A = A 
 		self.B = B
+
+		#solution matrix
 		self.X = [[-1 for _ in range(D)] for _ in range(self.N)]
 	
+	#calc minimum number of specific shift that all staff work on all Day
 	def calc_min(self, shift):
 		ans = float('inf')
 		for ID in range(self.N):
 			ans = min(ans, self.X[ID].count(shift))
 		
 		return ans
-
 	
+
+	#add at least A night shift to specific day
 	def add_night_shift(self, day):
 		num_Insert = 0
+		#add all night shift to prev day of day off for each person
 		for i in range(self.A):
 			for person in self.staff:
 				if self.X[person.ID-1][day-1] == -1:
@@ -27,8 +33,23 @@ class Solver:
 						num_Insert += 1
 						break 
 		
-		self.auto_fill(day, 4, num_Insert)
+		min_shift = self.calc_min(4)
+
+		# each slot in each day have at least A Staff
+		while num_Insert < self.A:
+			# loop through each person in staff
+			for person in self.staff:
+				if self.X[person.ID-1][day-1] == -1:
+
+					# add min shift to this person in given day
+					if self.X[person.ID-1].count(4) == min_shift:
+						self.X[person.ID-1][day-1] = 4
+						num_Insert += 1
+						break
+			
+			min_shift = self.calc_min(4)
 	
+	# add day off to X
 	def add_day_off(self, day):
 		for ID, person in enumerate(self.staff):
 			if day-2 < 0:
@@ -37,36 +58,25 @@ class Solver:
 			else:
 				if day in person.dayoff or self.X[ID][day-2] == 4:
 					self.X[ID][day-1] = 0
-	
-	def auto_fill(self, day, shift, num_Insert = 0):
-		
-		min_shift = self.calc_min(shift)
 
-		while num_Insert < self.A:
-			for _ in range(self.A-num_Insert):
-				for person in self.staff:
-					if self.X[person.ID-1][day-1] == -1:
-						
-						if self.X[person.ID-1].count(shift) == min_shift:
-							self.X[person.ID-1][day-1] = shift
-							num_Insert += 1
-							break
-			
-			min_shift = self.calc_min(shift)
 	
+	# fill every shift except night shift to specific day
 	def first_fill(self, day):
 		shift = 1
 		count_shift = 0
 		for person in self.staff:
+			# if that shift reach minimum requirement, go to next shift
 			if count_shift == self.A:
 				shift += 1
 				count_shift = 0
 			
+			# only add day shift, not night shift
 			if shift < 4:
 				if self.X[person.ID-1][day-1] == -1:
 					self.X[person.ID-1][day-1] = shift
 					count_shift += 1
 
+	# for each day, after reaching minimum requirement, fill all the shift one by one
 	def last_fill(self, day):
 		shift = 1
 		count_shift = self.A
@@ -80,11 +90,15 @@ class Solver:
 		
 
 
+	#main solve function
 	def solve(self):
 		for day in range(1, self.D+1):
+			#for each day, add night shift, and day off
 			self.add_night_shift(day)
 			self.add_day_off(day)
+			#fill all day shift to the minimum requirement
 			self.first_fill(day)
+			#fill shift to all remain staff
 			self.last_fill(day)
 			
 	
@@ -102,6 +116,7 @@ class Solver:
 
 
 def main():
+	#staff, D: number of Day, each slot in each day have at least A Staff and at most B staff
 	staff, D, A, B = import_data('1//test.txt')
 
 	sol = Solver(staff, D, A, B)
