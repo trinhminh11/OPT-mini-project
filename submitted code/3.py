d1eb1ec38a99cc15cafcd5f0e3f7c088aa468c3c
= <trinhtheminh1104@gmail.com>
2023-11-10 22:21:45 +0700

    Update sol for 3rd

--- submitted code/3.py
+++ submitted code/3.py
@@ -3,40 +3,14 @@ class Task:
 	def __init__(self, ID):
 		self.ID = ID
 		self.depth = 0
-		self.prev: list[Task] = []
-		self.next: list[Task] = []
+		self.num_prev = 0
+		self.prev = []
+		self.next = []
 		self.duration = 0
 		self.size = 1
 
 		# TimeDone
 		self.timeDone = -1
-	
-	# calc depth for all next tasks
-	def calc_depth(self):
-		for task in self.next:
-			task.depth = max(task.prev, key= lambda x: x.depth).depth + 1
-
-		for task in self.next:
-			task.calc_depth()
-	
-	def check_valid(self):
-		for task in self.prev:
-			if task.depth == 0:
-				self.remove_loop_branch()
-		
-		for task in self.next:
-			task.check_valid()
-	
-	def remove_loop_branch(self):
-		for task in self.prev:
-			try:
-				task.next.remove(self)
-			except:
-				pass
-
-		for task in self.next:
-			task.remove_loop_branch()
-
 	
 	# print for debug
 	def __str__(self):
@@ -49,14 +23,9 @@ class Worker:
 		self.cost_tasks = []
 
 		# dictinary with key = Task that this worker work on, value = Time Window to do this Task
-		self.works = {}
 
-		# work_flow of the worker
-		self.works_flow = [[0, self.start]]
+		self.last_work = 0
 	
-	def calc_work_flow(self):
-		self.works_flow = [[0, self.start]] + list(self.works.values())
-		self.works_flow.sort()
 	
 
 
@@ -69,24 +38,11 @@ def import_data():
 		tasks[i-1].next.append(tasks[j-1])
 
 		tasks[j-1].prev.append(tasks[i-1])
-	
-	# initial depth
-	for i in range(N):
-		if len(tasks[i].prev) == 0:
-			tasks[i].depth = 1
 
-	# calc all tasks depth
-	for task in tasks:
-		if task.depth == 1:
-			task.calc_depth()
-	
-	for task in tasks:
-		if task.depth == 1:
-			task.check_valid()
+		tasks[j-1].num_prev += 1
 
-	for task in tasks:
-		if task.depth == 1:
-			task.calc_depth()
+		tasks[j-1].depth += 1
+
 	
 	d = list(map(int, input().split()))
 
@@ -106,7 +62,7 @@ def import_data():
 	
 	for i in range(M):
 		workers[i].start = s[i]
-		workers[i].works_flow = [[0, s[i]]]
+		workers[i].last_work = s[i]
 		workers[i].cost_tasks = [-1 for _ in range(N)]
 	
 
@@ -125,114 +81,74 @@ Step 1:
 	Tree Depth is based on Q(i, j)
 	if Task j can only be started to execute after the completion of task i -> task i is parent of task j and task j is child of task i
 Step 2:
-	loop through each task, sort workers based on workflow[-1][-1] is start free time of that worker
-	loop through each worker, 
-	if this task depth is 1, meaning this task does not depend on any task, add the worker has minimum workflow[-1][-1] that can work on this task
-	else, calc timeDone of all task.prev (all tasks that this task depends on) and get the max timeDone
-		if we have a worker's workflow[-1][-1] less than max timeDone and can work on this task, get the worker that has the minimum cost
-		else, add the worker that has the minimum workflow[-1][-1] that can work on this task
+	loop through each task, sort workers based on last_work is the last working time of that worker
+	calc timeDone of all task.prev (all tasks that this task depends on) and get the max TimeDone
+		loop through each worker, 
+		add the worker have the maximum [max(last_work, max TimeDone), cost] to work on this task
 '''
 
 # Solver class
 class Solver:
 	def __init__(self, tasks, workers):
-		self.tasks = tasks
+		self._tasks = tasks
+		self.tasks = []
 		self.workers = workers
 	
-	# calc size for each task
-	def calc_size(self):
-		self.tasks.sort(key=lambda x: x.depth)
+	
+	def topo_task(self):
+		search_task = [task for task in self._tasks if task.depth == 0]
 
-		for task in self.tasks[::-1]:            
-			for t in task.prev:
-				t.size += task.size
+		while search_task:
+			current_task = search_task.pop(0)
+			self.tasks.append(current_task)
+
+			for task in current_task.next:
+				task.num_prev -= 1
+				if task.num_prev == 0:
+					search_task.append(task)
+
 	
 	# main solve function
 	def solve(self):
-		self.calc_size()
-		self.tasks.sort(key= lambda x: [x.depth])
+		self.topo_task()
 
-		# for task in self.tasks:
-		# 	print(task.ID, task.depth)
+		self.result = []
 
 		for task in self.tasks:
-			if task.depth == 0:
-				continue
+			max_time = -1
+			for t in task.prev:
+				if t.timeDone > max_time:
+					max_time = t.timeDone
 
-			self.workers.sort(key= lambda x: [x.works_flow[-1][-1], x.cost_tasks[task.ID-1]])
-			
-			# if depth of task is 1, just add the worker has minimum works_flow[-1][-1] that can work on this task
-			if task.depth == 1:
-				for worker in self.workers:
-					if worker.cost_tasks[task.ID-1] != -1:
-						worker.works[task] = [worker.works_flow[-1][-1], worker.works_flow[-1][-1] + task.duration]
-						task.timeDone = worker.works_flow[-1][-1] + task.duration
-						worker.calc_work_flow()
-						break
+			self.workers.sort(key= lambda x: [max(x.last_work, max_time), x.cost_tasks[task.ID-1], x.ID])
 
-			elif len(task.prev) == 0:
-				continue
-			else:
-				# calc max time of all task.prev
-				max_time = -1
-				for t in task.prev:
-
-					if t.timeDone > max_time:
-						max_time = t.timeDone
-					
-				best_cost = float('inf')
-
-				best_worker = self.workers[0]
-
-				for worker in self.workers:
-					if worker.works_flow[-1][-1] <= max_time:
-						if worker.cost_tasks[task.ID-1] != -1 and worker.cost_tasks[task.ID-1] < best_cost:
-							best_cost = worker.cost_tasks[task.ID-1]
-							best_worker = worker
-					else:
-						break
-				
-				if best_cost < float('inf'):
-					best_worker.works[task] = [max_time, max_time + task.duration]
-					task.timeDone = max_time + task.duration
-					best_worker.calc_work_flow()
-				else:
-					for worker in self.workers:
-						if worker.cost_tasks[task.ID-1] != -1:
-							worker.works[task] = [worker.works_flow[-1][-1], worker.works_flow[-1][-1] + task.duration]
-							task.timeDone = worker.works_flow[-1][-1] + task.duration
-							worker.calc_work_flow()
-							break
+			for worker in self.workers:
+				if worker.cost_tasks[task.ID-1] != -1:
+					task.timeDone = max(worker.last_work, max_time) + task.duration
+					worker.last_work = task.timeDone
+					self.result.append([task.ID, worker.ID, task.timeDone - task.duration])
+					break
+		
+		self.result.sort()
 				
 	def print_sol(self):
-		total = 0
-		ans = ""
+		print(len(self.result))
 
-		sol = [[] for _ in range(len(self.tasks))]
-
-		for worker in self.workers:
-			if len(worker.works_flow) > 1:
-				for task, time_window in worker.works.items():
-					sol[task.ID-1] = [task.ID, worker.ID, time_window[0]]
-		
-		for s in sol:
-			if s:
-				ans += f'{s[0]} {s[1]} {s[2]}\n'
-				total += 1
-		
-		print(total)
-		print(ans[:-1])
+		for result in self.result:
+			print(*result)
 	
 
 
 def main():
 	tasks, workers = import_data()
 
+
 	sol = Solver(tasks, workers)
 
 	sol.solve()
 
 	sol.print_sol()
+
 
 
 if __name__ == "__main__":
